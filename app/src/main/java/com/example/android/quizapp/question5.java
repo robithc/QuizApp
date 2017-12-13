b3c61740f01e7b359566ad5daa68230352cca858
robithc <robithc@gmail.com>
2017-12-13 10:46:40 +0200

    added 6 questions texts and layouts

--- app/src/main/java/com/example/android/quizapp/question5.java
+++ app/src/main/java/com/example/android/quizapp/question5.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class question4 extends AppCompatActivity {
+public class question5 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.question4);
+        setContentView(R.layout.question5);
     }
 
     public void onRadioButtonClicked(View view) {
@@ -19,12 +19,12 @@ public class question4 extends AppCompatActivity {
     }
 
     public void onClickNext(View view) {
-        Intent Intent = new Intent(this, question5.class);
+        Intent Intent = new Intent(this, question6.class);
         startActivity(Intent);
     }
 
     public void onClickBack(View view) {
-        Intent Intent = new Intent(this, question3.class);
+        Intent Intent = new Intent(this, question4.class);
         startActivity(Intent);
     }
 
--- app/src/main/java/com/example/android/quizapp/question6.java
+++ app/src/main/java/com/example/android/quizapp/question6.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class question4 extends AppCompatActivity {
+public class question6 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.question4);
+        setContentView(R.layout.question6);
     }
 
     public void onRadioButtonClicked(View view) {
@@ -19,12 +19,12 @@ public class question4 extends AppCompatActivity {
     }
 
     public void onClickNext(View view) {
-        Intent Intent = new Intent(this, question5.class);
+        Intent Intent = new Intent(this, question2.class);
         startActivity(Intent);
     }
 
     public void onClickBack(View view) {
-        Intent Intent = new Intent(this, question3.class);
+        Intent Intent = new Intent(this, question5.class);
         startActivity(Intent);
     }
 
